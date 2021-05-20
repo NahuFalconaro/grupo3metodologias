be99330d80ab831d6269e156ef14a84ee9db2113
Johana Infesta <johana.infesta@gmail.com>
2021-05-20 19:34:06 -0300

    G3-42 Formulario base para insertar informacion del solicitante

---
+++ TPE/config/ConfigApp.php
@@ -0,0 +1,13 @@
+<?php
+class ConfigApp
+{
+    
+    public static $ACTION ='action';
+    public static $PARAMS = 'params';
+    public static $ACTION = [
+        ''=>//indexControles#index,  // <- esto va a traer el index.tpl
+    ] 
+}
+
+
+?>
