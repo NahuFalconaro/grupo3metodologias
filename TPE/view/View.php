c9415c4696910d97e4ef881cfd66ef0dffc24df1
juanpedroarmella <juanpedroarmella@gmail.com>
2021-05-23 16:45:08 -0300

    G3-45 Usuario predefinido de secretaria agregado a la bbd. Backend de login secretaria hecho.

--- TPE/view/View.php
+++ TPE/view/View.php
@@ -2,23 +2,43 @@
 
 require_once "libs/smarty/Smarty.class.php";
 
-class View
-{
+class View{
 
-    function __construct()
-    {
+    function __construct(){
     }
 
-    function ShowHome()
-    {
+    function ShowHome(){
         $smarty = new Smarty();
         $smarty->assign('Titulo', 'Grupo 3 Metodologias');
         $smarty->display('templates/index.tpl');
     }
 
-    function Form()
-    {
+    function ShowLogin(){
+        $smarty = new Smarty();
+        $smarty->display('templates/ShowLogin.tpl');
+    }
+
+    function ShowHomeLogged(){
+        $smarty = new Smarty();
+        $smarty->display('templates/ShowHomeLogged.tpl');
+    }
+
+    function ShowErrorPass(){
+        $smarty = new Smarty();
+        $smarty->display('templates/ShowErrorPass.tpl');
+    }
+
+    function ShowErrorEmptyFields(){
+        $smarty = new Smarty();
+        $smarty->display('templates/ShowErrorEmptyFields.tpl');
+    }
+
+    function Form(){
         $smarty = new Smarty();
         $smarty->display('templates/formPedido.tpl');
     }
+
 }
+
+
+?>
